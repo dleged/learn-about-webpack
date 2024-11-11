5c35675c861582877aae16afe9783d0a2eecb445
dleged <15658866967@163.com>
2024-11-11 17:08:15 +0800

    docs: useing AI generate README file

---
+++ examples/chunk-graph/dist/c2_js.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["c2_js"],{
+
+/***/ "./c2.js":
+/*!***************!*\
+  !*** ./c2.js ***!
+  \***************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* export default binding */ __WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n\nconsole.log('c2 file');\n\n/* harmony default export */ function __WEBPACK_DEFAULT_EXPORT__() {\n  console.log('c2 function invoked')\n};\n\n\n\n//# sourceURL=webpack:///./c2.js?");
+
+/***/ })
+
+}]);
